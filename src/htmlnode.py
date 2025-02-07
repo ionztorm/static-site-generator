87338194fdf5e61c4b01c20b9cf62545abb29c7b
Leon Lonsdale <leon.codes@icloud.com>
2025-02-07 20:07:14 +0000

    ch2-l3: htmlnode

---
+++ src/htmlnode.py
@@ -0,0 +1,29 @@
+from __future__ import annotations
+
+from typing import override
+
+
+class HTMLNode:
+    def __init__(
+        self,
+        tag: str | None = None,
+        value: str | None = None,
+        children: HTMLNode | None = None,
+        props: dict[str, str] | None = None,
+    ) -> None:
+        self.tag: str | None = tag
+        self.value: str | None = value
+        self.children: HTMLNode | None = children
+        self.props: dict[str, str] | None = props
+
+    def to_html(self) -> str:
+        raise NotImplementedError
+
+    def props_to_html(self) -> str:
+        if not self.props:
+            return ""
+        return "".join(f" {key}='{value}'" for key, value in self.props.items())
+
+    @override
+    def __repr__(self) -> str:
+        return f"HTMLNode({self.tag}, {self.value}, children: {self.children}, {self.props})"

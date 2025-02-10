90654270d0d653179fd2db7e164132b15c9361b0
Leon Lonsdale <leon.codes@icloud.com>
2025-02-10 08:53:05 +0000

    ch3-l2: image & link extractor tests

---
+++ src/test_extract_markdown_images.py
@@ -0,0 +1,28 @@
+import unittest
+
+from markdown_parser import extract_markdown_images
+
+
+class TestExtractMarkdownImage(unittest.TestCase):
+    def test_extract_markdown_images_one_image(self) -> None:
+        markdown = "This text has an image ![alt text](https://www.example.com/image.jpg)"
+        matches = extract_markdown_images(markdown)
+        expected = [("alt text", "https://www.example.com/image.jpg")]
+        self.assertEqual(matches, expected)
+
+    def test_extract_markdown_images_two_images(self) -> None:
+        markdown = (
+            "This text has an image ![alt text](https://www.example.com/image.jpg)"
+            "and another ![alt text](https://www.example.com/image.jpg)"
+        )
+        matches = extract_markdown_images(markdown)
+        expected = [
+            ("alt text", "https://www.example.com/image.jpg"),
+            ("alt text", "https://www.example.com/image.jpg"),
+        ]
+        self.assertEqual(matches, expected)
+
+    def test_extract_markdown_images_links_no_images(self) -> None:
+        markdown = "This text has no images but has a link [link text](https://www.example.com)"
+        matches = extract_markdown_images(markdown)
+        self.assertEqual(matches, [])
---
+++ src/test_extract_markdown_links.py
@@ -0,0 +1,30 @@
+import unittest
+
+from markdown_parser import extract_markdown_links
+
+
+class TestExtractMarkdownLink(unittest.TestCase):
+    def test_extract_markdown_images_one_link(self) -> None:
+        markdown = "This text has an image [link text](https://www.example.com)"
+        matches = extract_markdown_links(markdown)
+        expected = [("link text", "https://www.example.com")]
+        self.assertEqual(matches, expected)
+
+    def test_extract_markdown_images_two_links(self) -> None:
+        markdown = (
+            "This text has an image [link text](https://www.example.com)"
+            "and another [link text](https://www.example.com)"
+        )
+        matches = extract_markdown_links(markdown)
+        expected = [
+            ("link text", "https://www.example.com"),
+            ("link text", "https://www.example.com"),
+        ]
+        self.assertEqual(matches, expected)
+
+    def test_extract_markdown_images_images_no_links(self) -> None:
+        markdown = (
+            "This text has no images but has a link ![alt text](https://www.example.com/image.jpg)"
+        )
+        matches = extract_markdown_links(markdown)
+        self.assertEqual(matches, [])

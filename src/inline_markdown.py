884f897f3f0067710dbe78479c1f05f3dee4f802
Leon Lonsdale <leon.codes@icloud.com>
2025-02-11 23:11:10 +0000

    minor refactoring

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -43,39 +43,51 @@ def split_nodes_by_type(
     extract_function: Callable[[str], list[tuple[str, str]]],
     text_type: TextType,
 ) -> list[TextNode]:
-    split_nodes: list[TextNode] = []
+    type_checked_nodes: list[TextNode] = []
 
     for node in nodes:
         if node.text_type != TextType.TEXT:
-            split_nodes.append(node)
+            type_checked_nodes.append(node)
             continue
 
-        node_string = node.text
-        elements = extract_function(node_string)
-        if not elements:
-            split_nodes.append(node)
-            continue
+        type_checked_nodes.extend(process_node(node, extract_function, text_type))
 
-        for display_text, source in elements:
-            segments = node_string.split(
-                f"[{display_text}]({source})"
-                if text_type == TextType.LINK
-                else f"![{display_text}]({source})"
-            )
+    return type_checked_nodes
 
-            if len(segments) != 2:
-                raise ValueError(f"Markdown syntax error for {display_text}.")
 
-            if segments[0] != "":
-                split_nodes.append(TextNode(segments[0], TextType.TEXT))
+def process_node(
+    node: TextNode,
+    extract_function: Callable[[str], list[tuple[str, str]]],
+    text_type: TextType,
+) -> list[TextNode]:
+    type_checked_nodes: list[TextNode] = []
 
-            split_nodes.append(TextNode(display_text, text_type, source))
-            node_string = segments[1]
+    node_string = node.text
+    elements = extract_function(node_string)
+    if not elements:
+        type_checked_nodes.append(node)
+        return type_checked_nodes
 
-        if node_string != "":
-            split_nodes.append(TextNode(node_string, TextType.TEXT))
+    for display_text, source in elements:
+        segments = node_string.split(
+            f"[{display_text}]({source})"
+            if text_type == TextType.LINK
+            else f"![{display_text}]({source})"
+        )
 
-    return split_nodes
+        if len(segments) != 2:
+            raise ValueError(f"Markdown syntax error for {display_text}.")
+
+        if segments[0] != "":
+            type_checked_nodes.append(TextNode(segments[0], TextType.TEXT))
+
+        type_checked_nodes.append(TextNode(display_text, text_type, source))
+        node_string = segments[1]
+
+    if node_string != "":
+        type_checked_nodes.append(TextNode(node_string, TextType.TEXT))
+
+    return type_checked_nodes
 
 
 def split_nodes_bold(nodes: list[TextNode]) -> list[TextNode]:
